37de717aa63d1ff7e0203843726f790b8ec1ca24
kilo <fraserkc@gmail.com>
2024-04-23 11:17:31 -0400

    Updates to footer and header

--- src/blocks/global/Header.tsx
+++ src/blocks/global/Header.tsx
@@ -13,13 +13,13 @@ export default async function Header() {
   return (
     <header className="container mx-auto py-4 px-6 flex justify-between items-center">
       <Image
-        src={header.logo.url}
-        width={header.logo.width / 10}
-        height={header.logo.height / 10}
-        alt={header.logo.alt}
+        src={header?.logo?.url}
+        width={header?.logo?.width / 10}
+        height={header?.logo?.height / 10}
+        alt={header?.logo?.alt}
       />
       <ul>
-        {header.navLinks.map((link: any, i: number) => (
+        {header?.navLinks?.map((link: any, i: number) => (
           <li key={link}>
             <Link href={link.link}>{link.label}</Link>
           </li>
