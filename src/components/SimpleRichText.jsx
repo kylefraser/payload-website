dfba7b1ef69dcad692b41bd7501c2f16f02ee046
kilo <fraserkc@gmail.com>
2024-04-23 14:36:57 -0400

    Back to (app) folder

--- src/components/SimpleRichText.jsx
+++
@@ -1,9 +0,0 @@
-import RichTextParser from '@/utils/RichTextParser'
-
-export default function SimpleRichText({ body }) {
-  return (
-    <div className="py-10 px-6">
-      <RichTextParser content={body} />
-    </div>
-  )
-}
