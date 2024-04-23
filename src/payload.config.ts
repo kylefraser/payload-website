fe7ff582c49b0aee6ac907ef7f31af3d47074393
kilo <fraserkc@gmail.com>
2024-04-23 11:51:35 -0400

    Update payload config

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -18,7 +18,6 @@ const filename = fileURLToPath(import.meta.url)
 const dirname = path.dirname(filename)
 
 export default buildConfig({
-  serverURL: 'http://localhost:3001',
   admin: {
     user: Users.slug,
   },
