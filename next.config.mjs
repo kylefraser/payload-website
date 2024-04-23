169c149f0bd97c2107fd29a1f38ea1ebfd201ab1
kilo <fraserkc@gmail.com>
2024-04-23 12:22:54 -0400

    test

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,14 @@ import { withPayload } from '@payloadcms/next/withPayload'
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // Your Next.js config here
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'payload-website-bay.vercel.app',
+      },
+    ],
+  },
 }
 
 export default withPayload(nextConfig)
