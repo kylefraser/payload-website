f1d7013a61ac1cd3c755f92d762cbd61320789eb
kilo <fraserkc@gmail.com>
2024-04-27 18:38:03 -0400

    Try incorporating Posthog

--- src/app/(app)/providers.tsx
+++ src/app/(app)/providers.tsx
@@ -1,7 +1,20 @@
 'use client'
 
 import { ThemeProvider } from 'next-themes'
+import posthog from 'posthog-js'
+import { PostHogProvider } from 'posthog-js/react'
+
+if (typeof window !== 'undefined') {
+  //@ts-expect-error
+  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
+    api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
+  })
+}
 
 export function Providers({ children }: any) {
-  return <ThemeProvider attribute="class">{children}</ThemeProvider>
+  return (
+    <ThemeProvider attribute="class">
+      <PostHogProvider client={posthog}>{children}</PostHogProvider>
+    </ThemeProvider>
+  )
 }
